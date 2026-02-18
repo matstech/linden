113cadef18d2a36890a97eff3a26acb0c86e4f7b
mat <matteo.stabile2@gmail.com>
2026-02-18 15:48:52 +0100

    chore: bump version
    - introducing CONTRIBUTING.md
    - init versifyr conf for version managemement

--- src/linden/__init__.py
+++ src/linden/__init__.py
@@ -7,8 +7,8 @@ This package provides tools for creating AI agents with:
 - Function calling support
 - Configurable agent behaviors
 """
-
-__version__ = "0.4.0"
+# $versifyr:template={{ (printf "__version__ = \"%s\""  .version) }}$
+__version__ = "0.5.0"
 __author__ = "matstech"
 __email__ = "matteo.stabile2@gmail.com"
 
