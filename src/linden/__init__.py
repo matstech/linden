7c477308dcc23b83edbf039cf515e60ab9716991
mat <matteo.stabile2@gmail.com>
2026-02-20 11:15:21 +0100

    ci: bump version

--- src/linden/__init__.py
+++ src/linden/__init__.py
@@ -8,7 +8,7 @@ This package provides tools for creating AI agents with:
 - Configurable agent behaviors
 """
 # $versifyr:template={{ (printf "__version__ = \"%s\""  .version) }}$
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 __author__ = "matstech"
 __email__ = "matteo.stabile2@gmail.com"
 
